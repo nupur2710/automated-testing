130bd397f2821f2617a94ce1dd9e486cdda5cfac
Nupur Gandhi <nupur@paperplane.net>
2017-07-14 18:23:42 +0530

    add dropdown and checkbox selection for test.assos

--- tests/dropdownSelection.js
+++ tests/dropdownSelection.js
@@ -1,39 +1,49 @@
 module.exports = {
-  'step one': function(browser) {
+  'Open browser and Select color': function(browser) {
     browser
-      .url('http://test.assos.com/')
+      .url('http://test.assos.com/TSHIRT')
       .resizeWindow(1200, 10000)
       .waitForElementVisible(
-        "a[href='/Men'].header-menu-level1-anchor",
-        5000,
+        '.product-views-option-dropdown-select',
+        2000,
         function() {
-          console.log('header found');
-          browser.moveToElement(
-            "a[href='/Men'].header-menu-level1-anchor",
-            1,
-            1,
-            function() {
-              browser.pause(2000);
-              console.log('element hovered');
-              browser.waitForElementVisible(
-                "a[href='/Men/Jerseys'].header-menu-level3-anchor",
-                2000
-              );
-              browser.click("a[href='/Men/Jerseys'].header-menu-level3-anchor");
-            }
+          browser.click(
+            "span[value='20'].product-views-option-color-picker-box"
           );
+          //select color
+          console.log('color selected');
+
+          browser.click(
+            'select[class="product-views-option-dropdown-select"] option[value="3"]'
+          );
+          console.log('size selected');
+          //selecting a particular value in the dropdown
+          //put the value in the value field
+
+          browser.click('.cart-add-to-cart-button-button', function() {
+            //this is not needed
+            //selecting color again to overcome the bug
+            console.log('add to cart');
+            browser.pause(2000);
+            browser.click(
+              "span[value='20'].product-views-option-color-picker-box"
+            );
+            console.log('color  selected');
+            browser.click('.cart-add-to-cart-button-button');
+            console.log('add to cart again');
+          });
         }
       );
     //putting data into callbacks because the elements will need time to get appended to the dom
   },
-  'step two': function(browser) {
-    console.log('the element clicked from hovered list clicked');
-    browser
-      .waitForElementVisible(
-        '.facets-browse-category-heading-main-description',
-        1000
-      )
-      .end();
-    console.log('the clicked section is visible');
-  }
 };
+
+//select the checkbox -
+//select the particular element that takes the click event
+// here span, coz the input and label are enclosed into it
+
+//select dropdown
+// give the value of the option you want to select in the dropdown
+
+//add to cart
+//click the add to cart button
